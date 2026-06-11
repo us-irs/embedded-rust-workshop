5a6a6914a70e517bc8676bbba4d6b90b4913814a
Robin Mueller <robin.mueller.m@pm.me>
2026-06-11 20:37:48 +0200

    finished first exercise

--- microbit-code/exercises/src/bin/blinky.rs
+++ microbit-code/exercises/src/bin/blinky.rs
@@ -11,13 +11,12 @@ async fn main(_spawner: Spawner) -> ! {
     // TODOs
     //
     // Step 1: Initialize the hardware by using the `embassy_nrf::init` method.
-    // Step 2: print some initial log to the console, for example the exercise name.
-    // Step 3: Create an Output GPIO driver for ROW1
-    // Step 4: Create an Output GPIO driver for COL1
-    // Step 5: Set the GPIOs to the correct state to drive LED D2, which is the LED in
-    //         the upper left corner of the LED matrix
-    // Step 6: Inside the empty loop, toggle the correct output to toggle the LED.
-    // Step 7: Led the LED blink with a certain frequency by using embassy_time::Timer
+    // Step 2: Print some initial log to the console, for example the exercise name.
+    // Step 3: Create Output GPIO drivers for ROW1 and COL1. You can use the datasheet provided at
+    //         the repo root or the images inside the exercise book to extract the relevant pin
+    //         information.
+    // Step 4: Inside the empty loop, toggle the correct output (only one is required) to toggle the LED
+    //         with a certain frequency by using embassy_time::Timer.
 
     loop {}
 }
