fecbf704c7dc16959e18a20644cf68a14ea961de
Robin Mueller <muellerr@irs.uni-stuttgart.de>
2026-07-01 15:24:38 +0200

    uart solution update

--- microbit-exercises/src/bin/uart_echo_solution.rs
+++ microbit-exercises/src/bin/uart_echo_solution.rs
@@ -5,11 +5,11 @@ use embassy_executor::Spawner;
 use embedded_io_async::Write;
 use exercises as _;
 
-use embassy_nrf::{peripherals, uarte};
+use embassy_nrf::{buffered_uarte, peripherals, uarte};
 
 embassy_nrf::bind_interrupts!(
     struct Irqs {
-        UARTE0 => uarte::InterruptHandler<peripherals::UARTE0>;
+        UARTE0 => buffered_uarte::InterruptHandler<peripherals::UARTE0>;
     }
 );
 
@@ -21,25 +21,30 @@ async fn main(_spawner: Spawner) -> ! {
     let mut uarte_config = uarte::Config::default();
     uarte_config.baudrate = uarte::Baudrate::Baud115200;
 
-    let uart = uarte::Uarte::new(
+    let mut driver_rx_buf: [u8; 256] = [0; 256];
+    let mut driver_tx_buf: [u8; 256] = [0; 256];
+    let uart = buffered_uarte::BufferedUarte::new(
         periphs.UARTE0,
+        periphs.TIMER0,
+        periphs.PPI_CH0,
+        periphs.PPI_CH1,
+        periphs.PPI_GROUP0,
         periphs.P1_08,
         periphs.P0_06,
         Irqs,
         uarte_config,
+        &mut driver_rx_buf,
+        &mut driver_tx_buf,
     );
-    let (mut uart_tx, mut uart_rx) =
-        uart.split_with_idle(periphs.TIMER0, periphs.PPI_CH0, periphs.PPI_CH1);
+    let (mut uart_rx, mut uart_tx) = uart.split();
     let mut rx_buf: [u8; 64] = [0; 64];
     loop {
-        match uart_rx.read_until_idle(&mut rx_buf).await {
+        match uart_rx.read(&mut rx_buf).await {
             Ok(read_bytes) => match uart_tx.write_all(&rx_buf[0..read_bytes]).await {
-                Ok(_) => {
-                    defmt::trace!("read {} bytes", &read_bytes);
-                }
-                Err(e) => defmt::error!("UART transmission error: {}", e),
+                Ok(_) => (),
+                Err(_e) => (),
             },
-            Err(e) => defmt::error!("UART reception error: {}", e),
+            Err(_e) => (),
         }
     }
 }
